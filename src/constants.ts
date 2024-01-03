9d49de4771409b97debc07f8618dbeb3bc9a7411
xoftex <airesearch@ai.xoftex.com>
2024-01-03 08:48:04 +0000

    Modified

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+import type { BubbleProps } from './features/bubble';
+
+export const defaultBotProps: BubbleProps = {
+  chatflowid: '',
+  apiHost: undefined,
+  chatflowConfig: undefined,
+  theme: undefined,
+};
